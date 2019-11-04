9fd3c5c17eba5dacda9e038c2c5362f00e484344
Suprava18 <54614920+Suprava18@users.noreply.github.com>
2019-11-04 21:17:27 +0530

    Updates

---
+++ precache-manifest.adceccc36a6223094acb53a913e843ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96974992064444cf07d5a5e5bc3278dc",
+    "url": "/happyshop/index.html"
+  },
+  {
+    "revision": "cbc661a30772d56fdc60",
+    "url": "/happyshop/static/css/2.9d6dfeae.chunk.css"
+  },
+  {
+    "revision": "3751eee8f1c3646a0410",
+    "url": "/happyshop/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "cbc661a30772d56fdc60",
+    "url": "/happyshop/static/js/2.cbf32cde.chunk.js"
+  },
+  {
+    "revision": "3751eee8f1c3646a0410",
+    "url": "/happyshop/static/js/main.29814bf4.chunk.js"
+  },
+  {
+    "revision": "cd1e8ecd90ddbc8508e8",
+    "url": "/happyshop/static/js/runtime-main.1c0dc448.js"
+  }
+]);
